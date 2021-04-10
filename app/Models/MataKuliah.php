594d8d51a9d309c311e62dfe4b57f389584424dc
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-04-11 06:02:42 +0700

    feat(models): matakuliah relation many to many

---
+++ app/Models/MataKuliah.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MataKuliah extends Model
+{
+    use HasFactory;
+    protected $table = 'matakuliah';
+    public function mahasiswa() {
+        return $this->belongsToMany(Mahasiswa::class, 'mahasiswa_matakuliah', 'mahasiswa_matakuliah', 'mahasiswa_id', 'matakuliah_id');
+    }
+}
